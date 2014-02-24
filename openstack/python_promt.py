417933b5384fffe477b1cec6074bfc6929e6eebf
ingenierouga <ingenierouga@gmail.com>
2014-02-24 14:59:43 -0600

    Create python_promt.py

---
+++ openstack/python_promt.py
@@ -0,0 +1,9 @@
+print "How old are you",
+age = raw_input()
+print "How Tall are you?",
+heigth = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, tou're %r old, %r tall and %r heavy." % (
+    age, height, weight)
