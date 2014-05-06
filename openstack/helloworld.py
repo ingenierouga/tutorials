11fc5e046a1b82bc57fa908b6a10d5bd8e061d75
Omar Chavez Orozco <omar.chavez.orozco@gmail.com>
2014-05-06 18:20:04 -0500

    Update helloworld.py

--- openstack/helloworld.py
+++ openstack/helloworld.py
@@ -1,34 +1,16 @@
+def print_values(a, b, c):
+    print "You're %r years old, %r m tall and %r kg." % (a, b, c)
 
-import unittest
-
-# Hello world
-def HelloWorld(s):
-    return "Hello, " + s + "!"
-
-def print_values(a,b,c):
-    print "So, you're %r years old, %r meters tall and %r Kg heavy." % (
-    a, b, c)
-
-# Unit test
-class HelloWorldTests(unittest.TestCase):
-
-    def testOne(self):
-        self.assertTrue(HelloWorld("Joe") == "Hello, Joe!")
-
-    def testTwo(self):
-        self.assertTrue(HelloWorld("world") == "Hello, world!")
 
 def main():
-#    unittest.main()
-#    print (HelloWorld("Emilio"))
-#    HelloWorld("Emilio2")
     print "How old are you",
     age = raw_input()
     print "How Tall are you?",
     height = raw_input()
     print "How much do you weigh?",
     weight = raw_input()
-    print_values(age,height,weight)
+    print_values(age, height, weight)
+
 
 if __name__ == '__main__':
-    main()	
+    main()
